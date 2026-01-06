31d710cf4c520c48bb399ac53bf13883396cd561
Bhavesh-Pathak <bhavesh.pathak1289@gmail.com>
2026-01-06 07:46:05 +0530

    fixed errors

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://gurukul-backend-1.onrender.com';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://gurukul-backend-kap2.onrender.com';
 
 export default API_BASE_URL;
