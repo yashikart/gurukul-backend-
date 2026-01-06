ccdcc3442d2b36d9acf02f072f9cab12677119e4
Bhavesh-Pathak <bhavesh.pathak1289@gmail.com>
2026-01-06 11:58:29 +0530

    Use BUILD_VERSION to prevent tree-shaking

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { initializeLanguageSupport } from './utils/languageSupport.js'
+import { BUILD_VERSION } from './config.js'
 
 // Initialize language support
 initializeLanguageSupport();
+
+// Log build version to prevent tree-shaking
+console.log('Gurukul Build Version:', BUILD_VERSION);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
